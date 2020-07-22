1cdebe310779c80c196ae3fe6da0dc271e25d4d9
Guillermo Alfaro <galfarotolon@gmail.com>
2020-07-22 18:03:44 -0500

    did moving_zeroes and product exercises

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -5,8 +5,29 @@ Returns: a List of integers
 def moving_zeroes(arr):
     # Your code here
 
-    pass
 
+    count = 0 # Count of non-zero elements 
+      
+    # Traverse the array. If element  
+    # encountered is non-zero, then 
+    # replace the element at index 
+    # 'count' with this element 
+    for i in range(len(arr)): 
+        if arr[i] != 0: 
+              
+            # here count is incremented 
+            arr[count] = arr[i] 
+            count+=1
+      
+    # Now all non-zero elements have been 
+    # shifted to front and 'count' is set 
+    # as index of first 0. Make all  
+    # elements 0 from count to end. 
+    while count < len(arr): 
+        arr[count] = 0
+        count += 1
+
+    return arr
 
 if __name__ == '__main__':
     # Use the main function here to test out your implementation
